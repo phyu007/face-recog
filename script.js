b513ad5fa050a543783494fd7d52e42b4688c67b
phyu007 <sow17809@gmail.com>
2020-03-21 15:39:07 +0800

    added face

--- script.js
+++ script.js
@@ -1,54 +1,6 @@
-// const video = document.getElementById('video')
-
-// Promise.all([
-//   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-//   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-//   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-//   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-// ]).then(startVideo)
-
-// function startVideo() {
-//   navigator.mediaDevices.getUserMedia(
-//     { video: {} },
-//     stream => video.srcObject = stream,
-//     err => console.error(err)
-//   )
-// }
-
-// document.querySelector('button').addEventListener('click', async (e) => {
-//   const stream = await navigator.mediaDevices.getUserMedia({
-//     video: true
-//   })
-//   document.querySelector('video').srcObject = stream
-// })
-
-
-// startVideo()
-
-// // video.addEventListener('play',() => {
-// //   console.log("video start")
-// // })
-
-// video.addEventListener('play', () => {
-//   const canvas = faceapi.createCanvasFromMedia(video)
-//   document.body.append(canvas)
-//   const displaySize = { width: video.width, height: video.height }
-//   faceapi.matchDimensions(canvas, displaySize)
-//   setInterval(async () => {
-//     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-//     console.log(detections)
-//     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-//     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-//     faceapi.draw.drawDetections(canvas, resizedDetections)
-//     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-//     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-//   }, 100)
-// })
-
-
 const imageUpload = document.getElementById('imageUpload')
 
-Promise.all([  //promise.all allows do everything below do async
+Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
@@ -66,14 +18,13 @@ async function start() {
   imageUpload.addEventListener('change', async () => {
     if (image) image.remove()
     if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0]) //await means ansyc 
+    image = await faceapi.bufferToImage(imageUpload.files[0])
     container.append(image)
     canvas = faceapi.createCanvasFromMedia(image)
     container.append(canvas)
     const displaySize = { width: image.width, height: image.height }
     faceapi.matchDimensions(canvas, displaySize)
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    document.body.append(detections.length)
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
     results.forEach((result, i) => {
@@ -84,15 +35,20 @@ async function start() {
   })
 }
 
-function loadLabeledImages() {
-  const labels = ['Phyu', 'KoKo', 'AKo', 'A ngel', 'Deen']
+
+
+function loadLabeledImages()
+{
+  const labels = ['KoKo','Phyu']
   return Promise.all(
-    labels.map(async label => {
-      const descriptions = []
-      for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://github.com/phyu007/face-recog/tree/master/labeled_images/${label}/${i}.jpg`)
+    labels.map(async label=> {
+      const descriptions  = []
+      for ( let i = 1 ; i < 2 ; i++)
+      {
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/phyu007/face-recog/master/labeled_images/${label}/${i}.PNG`)
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
         descriptions.push(detections.descriptor)
+
       }
 
       return new faceapi.LabeledFaceDescriptors(label, descriptions)
